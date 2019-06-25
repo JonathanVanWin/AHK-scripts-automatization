7f119b0f6f418accddcf9b5691032113d75cbffc
JonathanVanWin <grandearbol@gmail.com>
2019-06-25 12:36:48 +0300

    Added a github automatic repository script

---
+++ newRepo.bat
@@ -0,0 +1,11 @@
+set /p repo_name="Enter Repository name: "
+set /p isPrivate="Private? (false/true): "
+set /p descrip="Description of repository: "
+curl --data "{\"name\":\"%repo_name%\", \"auto_init\":true, \"has_wiki\":true, \"private\":%isPrivate%, \"description\":\"%descrip%\"}" -X POST -H "Authorization: token 8f0cf94817e9de9c63975998878232638337618f" https://api.github.com/user/repos
+git init
+set repo_name=%repo_name: =-%
+git remote add origin https://github.com/JonathanVanWin/%repo_name%.git
+git pull origin master --allow-unrelated-histories
+git add .
+git commit -a -m "Initial Commit to %repo_name%"
+git push -u origin --all
